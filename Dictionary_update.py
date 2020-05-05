aa38e8cbc648b7789c3a80308391220c077db4ad
Emmanuel Ivan <64347802+EmmanuelIvan@users.noreply.github.com>
2020-05-05 02:29:06 +0000

    Create Dictionary_update.py

---
+++ Dictionary_update.py
@@ -0,0 +1,5 @@
+#Veremos como actualizar datos de un diccionario
+clave = raw_input("Que clave deseas modificar: ")
+valor = raw_input("Dato a modificar: ")
+diccionario[clave] = valor
+print(diccionario)
